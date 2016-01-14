d51cd5b128b9fb065525a1bb54b4b35831f4dd06
mathiasvr <mathiasvr@gmail.com>
2016-01-14 02:50:37 +0100

    initial version

---
+++ README.md
@@ -0,0 +1,24 @@
+# Youtube Terminal [![npm][npm-image]][npm-url]
+
+[npm-image]: https://img.shields.io/npm/v/youtube-terminal.svg
+[npm-url]: https://www.npmjs.com/package/youtube-terminal
+
+[![experimental](http://hughsk.github.io/stability-badges/dist/experimental.svg)](http://github.com/hughsk/stability-badges)
+
+Stream youtube videos as ascii art in the terminal!
+
+## install
+
+```
+npm install youtube-terminal
+```
+
+## usage
+
+```js
+youtube-terminal 'cyanide and happiness'
+```
+
+## license
+
+MIT
---
+++ index.js
@@ -0,0 +1,56 @@
+var ytdl = require('ytdl-core')
+var Speaker = require('speaker')
+var youtubeSearch = require('youtube-crawler')
+var pcmAudio = require('./lib/pcm-audio')
+var asciiVideo = require('./lib/ascii-video')
+
+if (process.argv.length > 2) {
+  var query = process.argv.slice(2).join(' ')
+
+  // search youtube and play the first result
+  youtubeSearch(query, function (err, results) {
+    if (err) return console.error(err)
+    console.log('Playing:', results[0].title)
+    play(results[0].link)
+  })
+} else {console.log('yo')}
+
+function play (url) {
+  ytdl.getInfo(url, function (err, info) {
+    if (err) return console.error(err)
+
+    var audioItems = info.formats.filter(function (format) {
+      // audio only
+      return format.resolution === null
+    // format.type && format.type.startsWith('audio')
+    }).sort(function (a, b) {
+      // sort by audio quality
+      return b.audioBitrate - a.audioBitrate
+    })
+
+    var videoItems = info.formats.filter(function (format) {
+      // lowest resolution video only
+      return format.resolution === '144p' && format.audioBitrate === null
+    }).sort(function (a, b) {
+      // prefer webm
+      return a.container === 'webm' ? -1 : 1
+    })
+
+    var bestAudio = audioItems[0]
+
+    console.log('Audio quality: %s (%s)', bestAudio.audioBitrate + 'kbps', bestAudio.audioEncoding)
+
+    var speaker = new Speaker({
+      channels: 2,
+      bitDepth: 16,
+      sampleRate: bestAudio.audioEncoding === 'opus' ? 48000 : 44100
+    })
+
+    // play ascii video
+    asciiVideo(videoItems[0].url)
+
+    // TODO: avoid crude audio/video initial sync
+    // play audio
+    setTimeout(function () { pcmAudio(bestAudio.url).pipe(speaker) }, 250)
+  })
+}
